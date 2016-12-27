7e85b6fce2ba0bd7c4880211239970fc9549a2d5
Ola <ola.ahmad1993@hotmail.com>
2016-12-27 10:22:05 +0200

    updates

--- app.js
+++ app.js
@@ -14,16 +14,17 @@ var ObjectID = mongodb.ObjectID;
 var index = require('./routes/index');
 var contacts = require('./routes/contacts');
 let contact = require('./routes/contact');
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
