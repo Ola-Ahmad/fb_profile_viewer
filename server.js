dd9e35127de4e4891cb09b986a9b1aefbe7f7a5d
Ola <ola.ahmad1993@hotmail.com>
2016-12-28 01:17:38 +0200

    updates

--- server.js
+++ server.js
@@ -55,84 +55,8 @@ mongodb.MongoClient.connect(process.env.MONGOLAB_URI, function (err, database) {
     // Save database object from the callback for reuse.
     db = database;
     console.log("Database connection ready");
-    let port = normalizePort(process.env.PORT || '3000');
-    app.set('port', port);
-    /**
-     * Create HTTP server.
-     */
-    let server = http.createServer(app);
-    /**
-     * Listen on provided port, on all network interfaces.
-     */
-    server.listen(port);
-    server.on('error', onError);
-    server.on('listening', onListening);
-    /**
-     * Normalize a port into a number, string, or false.
-     */
-    function normalizePort(val) {
-        var port = parseInt(val, 10);
 
-        if (isNaN(port)) {
-            // named pipe
-            return val;
-        }
-        if (port >= 0) {
-            // port number
-            return port;
-        }
-        return false;
-    }
-    /**
-     * Event listener for HTTP server "error" event.
-     */
-    function onError(error) {
-        if (error.syscall !== 'listen') {
-            throw error;
-        }
-        let bind = typeof port === 'string'
-            ? 'Pipe ' + port
-            : 'Port ' + port;
-
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-            case 'EACCES':
-                console.error(bind + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                console.error(bind + ' is already in use');
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-    }
-    /**
-     * Event listener for HTTP server "listening" event.
-     */
-    function onListening() {
-        let addr = server.address();
-        let bind = typeof addr === 'string'
-            ? 'pipe ' + addr
-            : 'port ' + addr.port;
-        debug('Listening on ' + bind);
-    }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // CONTACTS API ROUTES BELOW
@@ -210,3 +134,66 @@ app.delete("/contacts/:id", function(req, res) {
     });
 });
 
+let port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+/**
+ * Create HTTP server.
+ */
+let server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
