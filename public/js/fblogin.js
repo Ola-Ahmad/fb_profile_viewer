5e4f1ae6857a8feeef24ee13a94d16da3905030e
Ola <ola.ahmad1993@hotmail.com>
2016-12-28 12:03:48 +0200

    getting data from fb

--- public/js/fblogin.js
+++ public/js/fblogin.js
@@ -34,7 +34,8 @@ function checkLoginState() {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me?fields=birthday,email,education,work,picture{url}', function(response) {
+    FB.api('/me',
+        {fields:"birthday,email,education,work,picture,gender"}, function(response) {
         console.log('Successful login for: ' + response.name);
         document.getElementById('status').innerHTML =
             'Thanks for logging in, ' + response.name + '!';
